dd9cad967b6a554b1ffc7e4e515b139ad10105b4
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2012-08-01 13:10:26 +0200

    Fix wrong outputs redirect

--- bin/pstarter.js
+++ bin/pstarter.js
@@ -1,25 +1,28 @@
 var pstarter = require('../lib/pstarter.js');
-console.log('Parent pid: ' + process.pid);
 pstarter.startMaster({}).startWorker(function() {
 	// run worker code
-	var r = parseInt(Math.random() * 10000) + 6000;
+	var r = parseInt(Math.random() * 20000) + 10000;
 	console.log('Hello world!');
 	console.log('will die in ' + r + ' ms');
+
 	setTimeout(function() {
-		throw Error('DADA');
+		if(Math.random() > 0.5) {
+			throw Error('DADA');
+		} else {
+			process.exit(1);
+		}
 	}, r);
 });
-
 /*
-// OR
-// master.js
-var pstarter = require('PATH_TO_LIB/pstarter.js');
-pstarter.startMaster(CONFIG_FILE, {exec : 'PATH_TO_WORKER/worker.js'})
+ // OR
+ // master.js
+ var pstarter = require('PATH_TO_LIB/pstarter.js');
+ pstarter.startMaster(CONFIG_FILE, {exec : 'PATH_TO_WORKER/worker.js'})
 
-// worker.js
-var pstarter = require('PATH_TO_LIB/pstarter.js');
-pstart.startWorker(function() {
-// Run worker code here
-});
-// or here
-*/
+ // worker.js
+ var pstarter = require('PATH_TO_LIB/pstarter.js');
+ pstart.startWorker(function() {
+ // Run worker code here
+ });
+ // or here
+ */
